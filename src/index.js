49a11ddfe3481026d706bdfdadd5b90dce513f77
grishaforsov <grisha.forsov@gmail.com>
2022-08-07 15:31:06 +0300

    feat: change towel sort

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-  if(Array.isArray(matrix)) {
-    matrix.map((elem, index) => {
-      index % 2 === 0
-      return elem.flat();
-    });
-  } else {
-    return elem.reverse().flat();
-  }
-
+  if(Array.isArray(matrix)) return matrix.map((elem, index) => index % 2 === 0 ? elem : elem.reverse()).flat();
   return [];
 }
+
 
